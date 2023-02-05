ca92bf5ea86f549bf79893ba9116886fdc24fe75
BABALLIKA <baballikav4@gmail.com>
2023-02-05 22:27:15 +0300

    kata

---
+++ src/CalculatorJM/LimCalculator.java
@@ -0,0 +1,41 @@
+package CalculatorJM;
+
+public class LimCalculator {
+    private int a;
+    private int b;
+
+    public LimCalculator(String formula){
+        String [] arr = formula.split(" ");
+            try {
+                a = Integer.parseInt(arr[0]);
+                b = Integer.parseInt(arr[2]);
+                if (a >= 1 && a <= 10 && b >= 1 && b <= 10){
+
+                    if(arr[1].equals("+")){
+                        System.out.println(a + b);
+                    }
+                    else if(arr[1].equals("-")){
+                        System.out.println(a - b);
+                    }
+                    else if (arr[1].equals("/")){
+                        double c = a;
+                        double d = b;
+                        System.out.println(c / d);
+                    }
+                    else if(arr[1].equals("*")){
+                        System.out.println(a * b);
+                    }
+
+                }
+                else {
+                    System.out.println("Некорректные данные");
+                }
+            }
+            catch (Exception exception){
+                System.out.println("Некорректный формат");
+            }
+
+
+
+    }
+}
---
+++ src/CalculatorJM/Main.java
@@ -0,0 +1,15 @@
+package CalculatorJM;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите формулу через пробел. Пример '2 + 2'");
+        Scanner scr = new Scanner(System.in);
+
+        String formula = scr.nextLine();
+        Formula formula1 = new Formula(formula);
+        LimCalculator limCalculator = new LimCalculator(formula);
+    }
+
+}
